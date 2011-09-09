1cb44d9a42dc7b9b74423c6603675b55c0b11647
Cyntax Tech <code@cyntaxtech.com>
2011-09-09 20:56:32 +0500

    added plugin files

---
+++ demo.php
@@ -0,0 +1,75 @@
+<?php
+	if (isset($_GET['get']) && $_GET['get'] == 'time') {
+		echo 'Current time on server is: ' . date("H:i:s") . ' (updated every 5 seconds)';
+		die();
+	}
+?>
+<div id="content">
+	<div class="centered" id="content-holder">
+		
+		<div id="main-content">
+		<h2 class="title demo">Timer Demo</h2>
+
+			<div id="test1">Timer attached to this element is paused. Use the buttons below to start/stop the timer.</div>
+			<div style="margin:10px">
+				<button onclick="$('#test1').timer('start');">Start Timer</button>
+				<button onclick="$('#test1').timer('stop');">Stop Timer</button>
+			</div>
+		
+<pre class="code">
+	&lt;div id="test1"&gt;Timer attached to this element is paused. Use the buttons below to start/stop the timer.&lt;/div&gt;
+	&lt;button onclick="$('#test1').timer('start');"&gt;Start Timer&lt;/button&gt;
+	&lt;button onclick="$('#test1').timer('stop');"&gt;Stop Timer&lt;/button&gt;
+	$('#test1').timer({
+		delay: 1000,
+		repeat: 15,
+		autostart: false,
+		callback: function( index ) {
+			html = "My timer has fired " + index + " times&lt;br /&gt;";
+			html += "Current client date time is: " + new Date();
+			$(this).html(html);
+		}
+	});
+</pre>
+		<div id="test2">You can see the current server time here after every 5 seconds</div>
+<pre class="code">
+	&lt;div id="test2"&gt;You can see the current server time here after every 5 seconds&lt;/div&gt;
+	$('#test2').timer({
+		delay: 5000,
+		repeat: true,
+		url: "demo?get=time"
+	});
+</pre>
+		</div>
+
+	</div>
+</div>
+<script language="javascript" type="text/javascript" src="jquery.min.js"></script>
+<script language="javascript" type="text/javascript" src="jquery.plugin.js"></script>
+<script language="javascript" type="text/javascript" src="jquery.timer.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	
+	// invoke a function on an element every second for 15 times
+	$('#test1').timer({
+		delay: 1000,
+		repeat: 15,
+		autostart: false,
+		callback: function( index ) {
+			html = "My timer has fired " + index + " times<br />";
+			html += "Current client date time is: " + new Date();
+			$(this).html(html);
+		}
+	});
+	
+	// load contents of a div from a url, after every 5 seconds
+	$('#test2').timer({
+		delay: 5000,
+		repeat: true,
+		url: "demo.php?get=time"
+	});
+
+});
+</script>
+
+
